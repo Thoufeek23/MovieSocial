8f5276223a668f168de82739c5ef282b2b1a4ceb
Thoufeek23 <mali21@binghamton.edu>
2025-11-26 21:12:39 -0500

    Temp moved fend

--- frontend/babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo', 'nativewind/babel'],
-    plugins: [
-      'react-native-reanimated/plugin', // Add reanimated (must be last)
-    ],
-  };
-};
